d0e18c0be1aa25beac73572aff57a6557151a12e
Uzoma Nwanne <uzoma.nwanne@gmail.com>
2022-09-16 14:22:02 -0700

    added product context and products to shop page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Shop from "./routes/shop/shop.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Navigation from "./routes/navigation/navigation.component";
 
@@ -9,7 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        {/* <Route path="shop" element={<Shop />} /> */}
+        <Route path="shop" element={<Shop />} /> 
         <Route path="auth" element={<Authentication/>} />
       </Route>
     </Routes>
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -20,6 +20,7 @@ const SignInForm = () => {
 
   const logGoogleUser = async () => {
     const {user} = await signInWithGooglePopup();
+   
   };
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
