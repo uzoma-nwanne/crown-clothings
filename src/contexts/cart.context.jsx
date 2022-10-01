9671dd1bdff14a444b9905bd369cfecc58ccf65c
Uzoma Nwanne <uzoma.nwanne@gmail.com>
2022-10-01 08:49:38 -0700

    updated cart quantity features

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,33 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
 
+export const addCartItem = (cartItems, productToAdd) => {
+  //if product already in cart update quantity
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+       cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem)
+    
+  }
+
+  //if product not in cart, add to cart and set quantity to 1
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 export const CartContext = createContext({
   isCartOpen: false,
-  //setIsOpen: () => {},
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const addItemToCart = (product) => setCartItems(addCartItem(cartItems, product));
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
