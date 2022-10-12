3daff32d11f4376cc1295709a1c2d7f329897cf6
Uzoma Nwanne <uzoma.nwanne@gmail.com>
2022-10-12 14:07:32 -0700

    added redux thunk to categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,9 +2,7 @@ import { useEffect } from 'react';
 import {Routes, Route} from 'react-router-dom';
 import { useDispatch } from 'react-redux/es/exports';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-
-import {setCategories} from '../../store/categories/categories.action';
+import {fetchCategoriesAsync} from '../../store/categories/categories.action';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
@@ -14,11 +12,7 @@ import "./shop.styles.scss";
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categories = await getCategoriesAndDocuments('categories');
-      dispatch(setCategories(categories));
-    }
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, [dispatch]);
   return (
     <Routes>
